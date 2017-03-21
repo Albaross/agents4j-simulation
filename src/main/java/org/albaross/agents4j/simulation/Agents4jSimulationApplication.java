8d67b5440288082dd4ba01d7fc9b53229d89e6ed
Manuel Barbi <manuel.barbi@tu-dortmund.de>
2017-03-21 19:12:08 +0100

    created SpringBoot Application

---
+++ src/main/java/org/albaross/agents4j/simulation/Agents4jSimulationApplication.java
@@ -0,0 +1,12 @@
+package org.albaross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agents4jSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agents4jSimulationApplication.class, args);
+	}
+}
---
+++ src/test/java/org/albaross/agents4j/simulation/Agents4jSimulationApplicationTests.java
@@ -0,0 +1,16 @@
+package org.albaross;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Agents4jSimulationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
